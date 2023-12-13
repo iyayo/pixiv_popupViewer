e7968bfca1138bc21ff6351762c01deb2226b2d4
iyayo <toraneko2525ry@gmail.com>
2023-12-13 18:19:31 +0900

    [update] キーボードショートカットからブックマークした場合にアラートを表示する

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.onload = async () => {
     let aTags;
+    let timerId = 0;
     const searchClass = ["_one-click-bookmark", "js-click-trackable", "opener", "item"];
     const observer = new MutationObserver(setEventHandler);
     const target = document.body;
@@ -29,11 +30,16 @@ window.onload = async () => {
     next_work_button.innerHTML = '<svg aria-hidden="true" width="24px" height="24px" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="white" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>';
     next_work_button.title = "次の作品";
 
+    const alert_bookmark = document.createElement("div");
+    alert_bookmark.id = "pixiv_popupViewer_alert_bookmark";
+    alert_bookmark.innerHTML = '<svg viewBox="0 0 32 32" width="72px" height="72px" class="sc-j89e3c-1 bXjFLc"><path d=" M21,5.5 C24.8659932,5.5 28,8.63400675 28,12.5 C28,18.2694439 24.2975093,23.1517313 17.2206059,27.1100183 C16.4622493,27.5342993 15.5379984,27.5343235 14.779626,27.110148 C7.70250208,23.1517462 4,18.2694529 4,12.5 C4,8.63400691 7.13400681,5.5 11,5.5 C12.829814,5.5 14.6210123,6.4144028 16,7.8282366 C17.3789877,6.4144028 19.170186,5.5 21,5.5 Z"></path><path d="M16,11.3317089 C15.0857201,9.28334665 13.0491506,7.5 11,7.5 C8.23857625,7.5 6,9.73857647 6,12.5 C6,17.4386065 9.2519779,21.7268174 15.7559337,25.3646328 C15.9076021,25.4494645 16.092439,25.4494644 16.2441073,25.3646326 C22.7480325,21.7268037 26,17.4385986 26,12.5 C26,9.73857625 23.7614237,7.5 21,7.5 C18.9508494,7.5 16.9142799,9.28334665 16,11.3317089 Z" class="sc-j89e3c-0 dUurgf"></path></svg>';
+    alert_bookmark.title = "ブックマーク";
+
     let options = await getStorageOptions();
     compressPopup(options.popupWidth, options.popupHeight);
 
     target.append(iframe_container);
-    iframe_container.append(iframe, switch_expand_button, previous_work_button, next_work_button);
+    iframe_container.append(iframe, switch_expand_button, previous_work_button, next_work_button, alert_bookmark);
 
     window.onstorage = function (e) {
         if (e.key !== "theme") return;
@@ -199,7 +205,16 @@ window.onload = async () => {
         if (request === "expand") switch_expand_button.click();
         if (request === "prev_work") previous_work_button.click();
         if (request === "next_work") next_work_button.click();
-        if (request === "add_bookmark" && iframe.contentWindow.document.querySelector("button.gtm-main-bookmark")) iframe.contentWindow.document.querySelector("button.gtm-main-bookmark").click();
+        if (request === "add_bookmark" && iframe.contentWindow.document.querySelector("button.gtm-main-bookmark")) {
+            iframe.contentWindow.document.querySelector("button.gtm-main-bookmark").click();
+            alert_bookmark.classList.add("show");
+
+            clearTimeout(timerId);
+
+            timerId = setTimeout(() => {
+                alert_bookmark.classList.remove("show");
+            }, 1000);
+        }
         if (request === "close_popup") iframe_container.click();
     })
 }
